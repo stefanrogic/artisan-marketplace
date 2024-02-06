f46223e260300faab64b03381b31ad77d541582d
stefanrogc <stefanrogic@protonmail.com>
2024-02-06 18:08:26 +0100

    fixed links on third bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,15 +8,16 @@ const Navbar = () => {
         <div className="flex flex-row items-center w-full border-t-2 pt-3 pb-2">
           <h1 className=" text-2xl font-bold cursor-pointer text-black">AM</h1>
           <div className="ms-10 hidden md:flex  flex-row items-center space-x-5 me-10 text-black">
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
-            <a href="#">Link</a>
+            <input className="border w-full" type="text" />
           </div>
           <button className="hidden md:block ms-auto bg-cyan-500 hover:bg-cyan-700 text-white px-5 py-2">Sign Up</button>
           <button className="block md:hidden ms-auto bg-cyan-500 hover:bg-cyan-700 text-white px-5 py-2">Menu</button>
         </div>
-        <div className="flex flex-row items-center w-full py-2 space-x-5 border-t-2">
+        <div className="flex flex-row justify-center items-center w-full py-2 space-x-5 border-t-2">
+          <a href="#">Link</a>
+          <a href="#">Link</a>
+          <a href="#">Link</a>
+          <a href="#">Link</a>
           <a href="#">Link</a>
           <a href="#">Link</a>
           <a href="#">Link</a>
